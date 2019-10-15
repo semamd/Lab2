17b4b2ed0b0eb7210fd3d455d48cafb283d9194c
xKrystalx <xKrystalx@users.noreply.github.com>
2019-10-15 14:12:38 +0200

    Initial commit 15.10.2019

---
+++ app/src/main/java/com/punky/lab2/MainActivity.java
@@ -0,0 +1,95 @@
+package com.punky.lab2;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        setButtonsClickListener();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void myClickerHandler(View view){
+        TextView txtV  = findViewById(R.id.main_text);
+        switch(view.getId()){
+            case R.id.button_blue:
+                txtV.setTextColor(Color.BLUE);
+                break;
+            case R.id.button_red:
+                txtV.setTextColor(Color.RED);
+                break;
+            case R.id.button_green:
+                txtV.setTextColor(Color.GREEN);
+                break;
+            default:
+        }
+    }
+
+    public void setButtonsClickListener(){
+        Button buttonred = findViewById(R.id.button_red);
+        Button buttonblue = findViewById(R.id.button_blue);
+        Button buttongreen = findViewById(R.id.button_green);
+
+        View.OnClickListener myClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                myClickerHandler(view);
+            }
+
+        };
+        buttonred.setOnClickListener(myClickListener);
+        buttonblue.setOnClickListener(myClickListener);
+        buttongreen.setOnClickListener(myClickListener);
+
+
+    }
+}
